568fae267d558c3f389bf04c92aadedadacabd46
jashan <jas123hanpreet@gmain.com>
2025-11-25 21:19:49 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import './App.css'
-import Navbar from './navbar.jsx'
-import Sidebar from './sidebar.jsx'
-import Contant from './contant.jsx'
+import '/src/App.css'
+import AppShall from './AppShall.jsx'
 
 function App() {
 
@@ -10,15 +8,7 @@ function App() {
     <>
       <div>
         <div className='h-screen w-full'>
-          <div className='flex flex-row scroll-m-0 fixed w-full'>
-            <Sidebar />
-            <Navbar />
-          </div>
-          <div className='pl-3 pb-14 pr7 sm:pl-32 pt-16 sm:pr-10'>
-            <div className=''>
-              <Contant />
-            </div>
-          </div>
+          <AppShall/>
         </div>
       </div>
     </>
--- src/AppShall.jsx
+++ src/AppShall.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import Navbar from './navbar.jsx'
-import Sidebar from './sidebar.jsx'
+import Navbar from './components/navbar.jsx'
+import Sidebar from './components/sidebar.jsx'
+import Contant from './contant.jsx'
 
 function AppShall() {
 
   return (
     <>
       <div>
-        <div className='flex flex-row'>
-          <Sidebar />
-          <Navbar />
-          
+        <div className='h-screen w-full'>
+          <div className='flex flex-row scroll-m-0 fixed w-full'>
+            <Sidebar />
+            <Navbar />
+          </div>
+          <div className='pl-3 pb-14 pr7 sm:pl-32 pt-16 sm:pr-10'>
+            <div className=''>
+              <Contant />
+            </div>
+          </div>
         </div>
-
       </div>
     </>
   )
