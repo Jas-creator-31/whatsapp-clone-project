5577550c938c7063771dd0ade4605188b70d03d0
jashan <jas123hanpreet@gmain.com>
2025-11-25 14:44:20 +0530

    safe addon 1

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <div className="relative w-lvw h-auto z-10">
             <div className="flex flex-row justify-between w-full bg-gray-950 p-2.5 align-middle">
-                <h1 className="text-gray-100 items-center justify-self-center">Logo</h1>
+                <h1 className="text-gray-100 items-center w-max">Logo</h1>
                 <div className="hidden shadow-gray-100 text-gray-100 sm:flex flex-row w-60 justify-between items-center">
                     <a className="hover:-translate-y-0.5 cursor-pointer">link 1</a>
                     <a className="hover:-translate-y-0.5 cursor-pointer">link 2</a>
