582000f4f89b56381e5404e9e145045a16f524dc
jashan <jas123hanpreet@gmain.com>
2025-11-26 22:53:02 +0530

    not muth

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import smImage from "../assets/pexels-andrew-3178786.jpg";
 
 export default function LoginPage() {
     return (
         <div className="font-sans">
             <div>
-                <div className="relative h-screen w-screen overflow-hidden bg-blue-500 ">
-                    <div className="h-screen w-screen bg-linear-to-b from-emerald-50 to-emerald-700 text-gray-950 flex flex-col">
+                <div className="relative h-screen w-screen overflow-hidden bg-blue-500 flex flex-row">
+                    <div className="h-screen flex-1 bg-linear-to-b from-emerald-50 to-emerald-700 text-gray-950 flex flex-col">
                         <div className="relative w-full h-25 flex flex-col justify-end-safe items-center">
-                            <h1 className="flex flex-col justify-end-safe h-full font-extrabold text-4xl text-gray-800 ">Welcome back</h1>
+                            <h1 className="flex flex-col justify-end-safe h-full font-extrabold text-4xl text-gray-800">Welcome back</h1>
                         </div>
                         <div className="relative h-70 min-w-svw flex flex-col p-2.5 top-16 justify-around">
                             <div className="flex flex-col justify-center align-middle mt-5">
@@ -38,12 +39,13 @@ export default function LoginPage() {
 
                             <div className="flex flex-row justify-center mt-5">
                                 <p>Don't have a account?</p>
-                                <a href="" className="ml-3 text-amber-200 hover:-translate-y-1">Sign up</a>
+                                <a href="" className="ml-3 text-amber-200 hover:-translate-y-1 contrast-100">Sign up</a>
                             </div>
                             
                         </div>
 
                     </div>
+                    <img className=" sm:block h-screen w-80 object-cover" src={smImage} alt="illustration" height={100} width={50}/>
                 </div>
             </div>
         </div>
