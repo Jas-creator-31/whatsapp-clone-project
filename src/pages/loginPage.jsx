01c98051eef802643b126bda5604eb3ed7e51319
jashan <jas123hanpreet@gmain.com>
2025-11-27 14:51:06 +0530

    Refactor LoginPage component for improved styling and accessibility

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import smImage from "../assets/pexels-andrew-3178786.jpg";
 
 
 
@@ -7,17 +6,17 @@ export default function LoginPage() {
     return (
         <div className="font-sans">
             <div>
-                <div className="relative h-screen w-screen overflow-hidden bg-blue-500 flex flex-row">
-                    <div className="h-screen flex-1 bg-linear-to-b from-emerald-50 to-emerald-700 text-gray-950 flex flex-col">
+                <div className="relative h-screen w-screen overflow-hidden flex flex-row">
+                    <div className="h-screen flex-1 bg-linear-to-b from-sky-50 to-blue-700 text-gray-950 flex flex-col">
                         <div className="relative w-full h-25 flex flex-col justify-end-safe items-center">
                             <h1 className="flex flex-col justify-end-safe h-full font-extrabold text-4xl text-gray-800">Welcome back</h1>
                         </div>
-                        <div className="relative h-70 min-w-svw flex flex-col p-2.5 top-16 justify-around">
+                        <div className="relative h-70 w-max flex flex-col p-2.5 top-16 justify-around self-center">
                             <div className="flex flex-col justify-center align-middle mt-5">
                                
                                 <div className="h-max w-full flex flex-col justify-center items-center">
                                     <p className="text-left w-60">Email Address</p>
-                                    <input className="w-60 h-10 rounded-2xl bg-green-800 p-1" type="email" id="emailInput" placeholder="Enter your email" />
+                                    <input className="w-60 h-10 rounded-2xl bg-sky-100 p-1 focus:outline-2 focus:outline-blue-500" type="email" id="emailInput" placeholder="Enter your email" />
                                 </div>
 
                             </div>
@@ -25,29 +24,28 @@ export default function LoginPage() {
                             <div className="flex flex-col justify-center align-middle mt-5">
                                 <div className="h-max w-full flex flex-col justify-center items-center">
                                     <p className="text-left w-60">Password</p>
-                                    <input className="w-60 h-10 rounded-2xl bg-green-800 p-1 flex flex-row justify-self-center " type="password" id="password" placeholder="Password" />
+                                    <input className="w-60 h-10 rounded-2xl bg-sky-100 p-1 flex flex-row justify-self-center focus:outline-2 focus:outline-blue-500" type="password" id="password" placeholder="Password" />
                                 </div>
                             </div>
 
                             <div className="flex flex-col justify-center align-middle mt-5">
                                 <div className="h-max w-full flex flex-col justify-center items-center">
                                     <p className="text-left w-60">Confirme Password</p>
-                                    <input className="w-60 h-10 rounded-2xl bg-green-800 p-1 flex flex-row justify-self-center " type="password" id="passwordConfirme" placeholder="Confirme password" />
+                                    <input className="w-60 h-10 rounded-2xl bg-sky-100 p-1 flex flex-row justify-self-center focus:outline-2 focus:outline-blue-500" type="password" id="passwordConfirme" placeholder="Confirme password" />
                                 </div>
 
                             </div>
 
-                            <button className="bg-green-800 p-2 w-60 h-10 rounded-2xl self-center mt-10" type="submit">Ligin</button>
+                            <button className="bg-blue-600 p-2 w-60 h-10 rounded-2xl self-center mt-10 hover:bg-blue-700 hover:cursor-pointer" type="submit">Login</button>
 
                             <div className="flex flex-row justify-center mt-5">
                                 <p>Don't have a account?</p>
-                                <a href="" className="ml-3 text-amber-200 hover:-translate-y-1 contrast-100">Sign up</a>
+                                <a href="" className="ml-3 text-amber-200 hover:-translate-y-1 contrast-100 hover:underline">Sign up</a>
                             </div>
                             
                         </div>
 
                     </div>
-                    <img className=" sm:block h-screen w-80 object-cover" src={smImage} alt="illustration" height={100} width={50}/>
                 </div>
             </div>
         </div>
