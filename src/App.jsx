e348e8b6dd942e176c52e426c6c22559b3d73532
jashan <jas123hanpreet@gmain.com>
2025-11-29 14:15:29 +0530

    Add ChatBotPage component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '/src/App.css'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
+import ChatBotPage from './pages/chatBotPage'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/chatbot' element={<ChatBotPage/>}/>
       </Routes>
     </>
   )
---
+++ src/pages/chatBotPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ChatBotPage() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>chat Bot Page</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
