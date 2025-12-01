bd1113b90e8156e3d5f5724ccb12ac3108db0310
jashan <jas123hanpreet@gmain.com>
2025-12-01 18:11:59 +0530

    refactor: remove unused components and consolidate routing to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import './App.css'
-import HomePage from './pages/homePage'
-import LoginPage from './pages/loginPage'
-import ChatBotPage from './pages/chatBotPage'
 import ReactInteractivityTestPage from './pages/reactInteractivityTestPage'
-import WhatsappMokeupPage from './pages/whatsappMokeupPage'
+import Home from './pages/home'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -12,11 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/chatbot' element={<ChatBotPage/>}/>
-        <Route path='/reacttestpage' element={<ReactInteractivityTestPage/>}/>
-        <Route path='/whatsapp' element={<WhatsappMokeupPage />} />
+        <Route path='/' element={<Home/>} />
       </Routes>
     </>
   )
--- src/AppShall.jsx
+++ src/AppShall.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Navbar from './components/navbar.jsx'
-import Sidebar from './components/sidebar.jsx'
-// import Contant from './contant.jsx'
 
 function AppShall() {
 
@@ -10,8 +7,7 @@ function AppShall() {
       <div>
         <div>
           <div className='flex flex-row scroll-m-0 fixed w-full'>
-            <Sidebar />
-            <Navbar />
+            
           </div>
           
         </div>
