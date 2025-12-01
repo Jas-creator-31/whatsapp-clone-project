e66215e12f79d51d0ef58ada56a5083b225aec71
jashan <jas123hanpreet@gmain.com>
2025-12-01 16:03:36 +0530

    Refactor ReactInteractivityTestPage to use a dedicated counter function and add tailwind.config.js for Tailwind CSS setup

--- src/pages/reactInteractivityTestPage.jsx
+++ src/pages/reactInteractivityTestPage.jsx
@@ -4,12 +4,16 @@ export default function ReactInteractivityTestPage() {
 
     const [count, setCount] = useState(0);
 
+    function counter() {
+        setCount(p => p + 1)
+    }
+
     return (
         <div>
             <div>
                 <div>
                     <h1>Click button below</h1>
-                    <button onClick={() => setCount() + 1}>Click me!</button>
+                    <button type="button" onClick={counter}>Click me!</button>
                     <p>You clicked me {count} times</p>
                 </div>
             </div>
