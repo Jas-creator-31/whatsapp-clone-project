a4145cf22e4355750518efe9e988f38f458c9ca6
jashan <jas123hanpreet@gmain.com>
2025-12-01 17:47:52 +0530

    nothing

--- src/pages/whatsappMokeupPage.jsx
+++ src/pages/whatsappMokeupPage.jsx
@@ -7,10 +7,10 @@ export default function WhatsappMokeupPage() {
     return (
         <div className="h-screen w-screen">
             <div className="h-min w-min">
-                <div className="grid grid-cols-12 grid-rows-1 h-screen w-screen">
-                    <div className="col-span-1 h-screen "></div>
-                    <div className="col-start-2 col-end-6"></div>
-                    <div className="col-start-7 col-end-12"></div>
+                <div className="md:grid md:grid-cols-50 md:grid-rows-1 h-screen w-screen flex flex-col">
+                    <div className="w-screen h-5 fixed z-50 bottom-0 bg-gray-700 md:col-span-3 md:h-screen md:w-16 md:"></div>
+                    <div className="md:col-start-3 md:col-end-23 md:w-123.1 bg-amber-300"></div>
+                    <div className="md:col-start-23 md:col-end-51 md:w-182 bg-red-800"></div>
                 </div>
             </div>
         </div>
