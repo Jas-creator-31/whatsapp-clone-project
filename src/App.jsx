7ea5c468d5b5621a3063f5d62370edab48db2970
jashan <jas123hanpreet@gmain.com>
2025-12-01 16:32:05 +0530

    sjlfgrhdfjhfdjvn

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import ChatBotPage from './pages/chatBotPage'
 import ReactInteractivityTestPage from './pages/reactInteractivityTestPage'
+import WhatsappMokeupPage from './pages/whatsappMokeupPage'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/chatbot' element={<ChatBotPage/>}/>
         <Route path='/reacttestpage' element={<ReactInteractivityTestPage/>}/>
+        <Route path='/whatsapp' element={<WhatsappMokeupPage />} />
       </Routes>
     </>
   )
---
+++ src/pages/whatsappMokeupPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function WhatsappMokeupPage() {
+
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
